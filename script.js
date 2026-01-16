5c06cb277ca2744066846ff3495e01df0410f363
Lucas João <126522724+Lucasjoao9@users.noreply.github.com>
2026-01-15 22:00:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -29,10 +29,46 @@ window.addEventListener('scroll', () => {
 window.addEventListener("scroll", () => {
   const header = document.querySelector(".site-header");
 
-  if (window.scrollY > 90) {
+  if (window.scrollY > 80) {
     header.classList.add("shrink");
   } else {
     header.classList.remove("shrink");
   }
 });
+
+
+let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+
+function adicionarCarrinho(nome, preco) {
+  carrinho.push({ nome, preco });
+  localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  alert("Produto adicionado ao carrinho!");
+}
+
+function finalizarPedido() {
+  if (carrinho.length === 0) {
+    alert("Seu carrinho está vazio.");
+    return;
+  }
+
+  let mensagem = "Olá! Gostaria de fazer o pedido:%0A%0A";
+  let total = 0;
+
+  carrinho.forEach((item, index) => {
+    mensagem += `${index + 1}. ${item.nome} - R$ ${item.preco.toFixed(2)}%0A`;
+    total += item.preco;
+  });
+
+  mensagem += `%0A*Total:* R$ ${total.toFixed(2)}`;
+
+  const telefone = "554791631238"; // <-- SEU WHATS AQUI
+  const url = `https://wa.me/${telefone}?text=${mensagem}`;
+
+  window.open(url, "_blank");
+
+  localStorage.removeItem("carrinho");
+  carrinho = [];
+}
+
+
 
